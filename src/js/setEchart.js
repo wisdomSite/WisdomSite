0b95087381f12d711d69b1934e5a3d4ca4297fbc
yangpeng <yangpeng>
2018-10-24 18:05:52 +0800

    merge

--- src/js/setEchart.js
+++ src/js/setEchart.js
@@ -1,5 +1,30 @@
-var qualityChart = document.getElementById('quality-chart');
-var qualityChartOpts = {
+// var qualityChart = document.getElementById('quality-chart');
+// var qualityChartOpts = {
 
+// }
+// var chart = echarts.init(qualityChart,'dark')
+
+/**
+ * 
+ * @param {*} ele 容器选择器
+ * @param {*} options echarts配置参数
+ */
+let setChart = function(ele,options) {
+    let _defalut = {//默认配置参数
+
+    }
+ 
+    console.log(typeof echarts === 'object')
+    if(typeof echarts === 'object') {
+        console.log('查看echarts',echarts)
+        console.log('ele',ele)
+        
+        let myChart = echarts.init(ele)
+        myChart.setOption(options)
+        return myChart
+    }
+    
 }
-var chart = echarts.init(qualityChart,'dark')
+//==========================================================
+
+//==========================================================
